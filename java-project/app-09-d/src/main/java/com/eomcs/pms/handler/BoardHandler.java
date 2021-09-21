fc680419c11d10eab2284e51014f375d290d74a3
leeseoeun <seoeun.shuen@gmail.com>
2021-09-22 03:04:15 +0900

    app-09-d

---
+++ java-project/app-09-d/src/main/java/com/eomcs/pms/handler/BoardHandler.java
@@ -0,0 +1,106 @@
+package com.eomcs.pms.handler;
+
+import java.sql.Date;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.util.Prompt;
+
+public class BoardHandler {
+
+  //객체는 생성 후(new) 사용 가능
+  BoardList boardList = new BoardList();
+
+  public void add() {
+    Board board = new Board();
+    System.out.println("[새 게시글]");
+    board.no = Prompt.inputInt("번호? ");
+    board.title = Prompt.inputString("제목? ");
+    board.content = Prompt.inputString("내용? ");
+    board.writer = Prompt.inputString("작성자? ");
+    board.registeredDate = new Date(System.currentTimeMillis());
+
+    boardList.add(board);
+  }
+
+  public void list() {
+    System.out.println("[게시글 목록]");
+
+    Board[] list = boardList.toArray();
+
+    for (Board board : list) {
+      System.out.printf("%d, %s, %s, %s, %d\n",
+          board.no,
+          board.title,
+          board.writer,
+          board.registeredDate,
+          board.viewCount,
+          board.like);
+    }
+  }
+
+  public void detail() {
+    System.out.println("[게시글 상세보기]");
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = boardList.findByNo(no);
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    System.out.printf("제목: %s\n", board.title);
+    System.out.printf("내용: %s\n", board.content);
+    System.out.printf("작성자: %s\n", board.writer);
+    System.out.printf("등록일: %s\n", board.registeredDate);
+    System.out.printf("조회수: %d\n", ++board.viewCount);
+  }
+
+  public void update() {
+    System.out.println("[게시글 변경]");
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = boardList.findByNo(no);
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    String title = Prompt.inputString(String.format("제목(%s)? ", board.title));
+    String content = Prompt.inputString(String.format("내용(%s)? ", board.content));
+
+    String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+      System.out.println("게시글 변경을 취소하였습니다.");
+      return;
+    }
+
+    board.title = title;
+    board.content = content;
+    System.out.println("게시글을 변경하였습니다.");
+  }
+
+  public void delete() {
+    System.out.println("[게시글 삭제]");
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = boardList.findByNo(no);
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
+    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+      System.out.println("게시글 삭제를 취소하였습니다.");
+      return;
+    }
+
+    boardList.remove(board);
+
+    System.out.println("게시글을 삭제하였습니다.");
+  }
+
+
+}
