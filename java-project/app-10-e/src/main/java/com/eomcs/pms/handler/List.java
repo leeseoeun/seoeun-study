5e09c1c4d5410b68fe7f1744929182386c361752
leeseoeun <seoeun.shuen@gmail.com>
2021-09-22 19:03:48 +0900

    app-10-f

---
+++ java-project/app-10-e/src/main/java/com/eomcs/pms/handler/List.java
@@ -0,0 +1,12 @@
+package com.eomcs.pms.handler;
+
+public abstract class List {
+
+  public abstract void add(Object item);
+
+  public abstract Object[] toArray();
+
+  public abstract boolean remove(Object item);
+
+
+}
